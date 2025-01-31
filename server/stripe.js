0a2bf47d5cd139679061fba0bd0ecc0473969044
Darshan Kumar <darshandachu8385@gmail.com>
2025-01-31 14:20:36 +0530

    Add files via upload

---
+++ server/stripe.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+app.use(express.json());
+app.use(cors());
+
+// checkout api
+app.post("/api/create-checkout-session",async(req,res)=>{
+    const {products} = req.body;
+
+
+    const lineItems = products.map((product)=>({
+        donation_data:{
+            currency:"inr",
+            donation_dat:{
+                name:pets.donate,
+                images:[pets.imgdata]
+            },
+            unit_amount:product.price * 100,
+        },
+        quantity:unit_amount
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        line_items:lineItems,
+        mode:"payment",
+        success_url:"http://localhost:3000/sucess",
+        cancel_url:"http://localhost:3000/cancel",
+    });
+
+    res.json({id:session.id})
+ 
+})
+
